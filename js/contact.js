b19eb0740ef77f95432999fb8e37a5c4a8255664
HMAsp <hanand55935@stud.noroff.no>
2023-05-02 20:20:40 +0200

    form validation complete
    have left to figure out the success and error messages

--- js/contact.js
+++ js/contact.js
@@ -6,16 +6,70 @@ burgerFunction();
 
 const formContainer = document.querySelector(".formContainer");
 const fullName = document.querySelector("#name");
-// const nameLabel = document.querySelector(".nameLabel");
+
+const nameCheckmark = document.querySelector(".nameCheckmark");
+const emailCheckmark = document.querySelector(".emailCheckmark");
+const subjectCheckmark = document.querySelector(".subjectCheckmark");
+const messageCheckmark = document.querySelector(".messageCheckmark");
 
 const email = document.querySelector("#email");
 const subject = document.querySelector("#subject");
-const message = document.querySelector("#message");
+const message = document.querySelector(".formMsg");
 
+const formBtn = document.querySelector(".formBtn");
+
+//CHECKS IF NAME LENGTH IS 5 MINIMUM
 fullName.addEventListener("input", function () {
-  const nameLabel = document.querySelector(".nameLabel");
-
   if (checkLength(fullName.value, 4)) {
-    nameLabel.innerHTML = "red";
+    nameCheckmark.classList.add("valid");
+  } else {
+    nameCheckmark.classList.remove("valid");
   }
+});
+
+//CHECKS IF EMAIL IS CORRECTLY FORMATTED
+email.addEventListener("input", function () {
+  if (validateEmail(email.value)) {
+    emailCheckmark.classList.add("valid");
+  } else {
+    emailCheckmark.classList.remove("valid");
+  }
+});
+
+//CHECKS IF SUBJECT LENGTH IS 15 MINIMUM
+subject.addEventListener("input", function () {
+  if (checkLength(subject.value, 14)) {
+    subjectCheckmark.classList.add("valid");
+  } else {
+    subjectCheckmark.classList.remove("valid");
+  }
+});
+
+//CHECKS IF MESSAGE LENGTH IS 25 MINIMUM
+message.addEventListener("input", function () {
+  if (checkLength(message.value, 25)) {
+    messageCheckmark.classList.add("valid");
+  } else {
+    messageCheckmark.classList.remove("valid");
+  }
+});
+
+//CHECKS IF ALL INPUTS ARE VALID AND SUBMITS WITH SUCCESS MESSAGE OR FAILS WITH ERROR MESSAGE
+
+function validateForm() {
+  if (
+    checkLength(fullName.value, 4) &&
+    validateEmail(email.value) &&
+    checkLength(subject.value, 14) &&
+    checkLength(message.value, 25)
+  ) {
+    console.log("Validation success");
+  } else {
+    console.log("Incomplete");
+  }
+}
+
+formBtn.addEventListener("click", function (event) {
+  validateForm();
+  event.preventDefault();
 });
