c0ed0f2c2f33278a11cce563e983aabec107354b
HMAsp <hanand55935@stud.noroff.no>
2023-05-23 11:54:18 +0200

    cleaning code

--- js/about.js
+++ js/about.js
@@ -1,9 +1,7 @@
-import { burgerFunction } from "./global/functions.js";
 import { subInputs } from "./global/functions.js";
 import { preventSubDefaultReload } from "./global/functions.js";
 import { navFilterToggle } from "./global/functions.js";
 
-burgerFunction();
 subInputs();
 preventSubDefaultReload();
 navFilterToggle();
